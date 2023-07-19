c27e2b4a5dd3b6116ccfe38d70f03b84fa847a6f
MONTASIM <mmamun@mymedicalhub.com>
2023-07-19 20:58:52 +0600

    [ADDED] device orientation detection

--- src/js/script.js
+++ src/js/script.js
@@ -35,16 +35,12 @@ console.log({defaultDeviceHeight});
 console.log({deviceWidth});
 console.log({deviceHeight});
 
-screen.orientation.addEventListener("change", function(e) {
-    let deviceOrientation = window.matchMedia("(orientation: portrait)");
-
-    deviceOrientation.addEventListener("change", function(e) {
-        if(e.matches) {
-            // Portrait mode
-            console.log('Portrait');
-        } else {
-            // Landscape
-            console.log('Landscape');
-        }
-    })
+window.addEventListener("orientationchange", function() {
+    if (window.orientation === 0) {
+        // Portrait mode
+        console.log('Portrait');
+    } else {
+        // Landscape mode
+        console.log('Landscape');
+    }
 });
